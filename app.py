32a8cd4a14699d09f3263cf40f2aa2cb56870a9e
spink <spink@metaverseholdings.tech>
2021-12-10 12:43:11 +0000

    init

---
+++ app.py
@@ -0,0 +1,120 @@
+"""
+This app creates a simple sidebar layout using inline style arguments and the
+dbc.Nav component.
+
+dcc.Location is used to track the current location, and a callback uses the
+current location to render the appropriate page content. The active prop of
+each NavLink is set automatically according to the current pathname. To use
+this feature you must install dash-bootstrap-components >= 0.11.0.
+
+For more details on building multi-page Dash applications, check out the Dash
+documentation: https://dash.plot.ly/urls
+"""
+from flask import Flask
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html
+import pandas as pd
+
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P("A simple sidebar layout with navigation links", className="lead"),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return toucanrarity()
+    elif pathname == "/page-1":
+        return html.P("This is the content of page 1. Yay!")
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+def toucanrarity():
+    """
+    this is a page that displays the toucan rarity
+    """
+    toucan_rarity = pd.read_csv("toucans_rarity.csv")
+
+    toucan_rarity = toucan_rarity[
+        [
+            "name",
+            "Eyes",
+            "Plumage",
+            "Beak",
+            "Hat",
+            "Clothes",
+            "Accessories",
+            "trait_n",
+            "mean_trait_rarity",
+        ]
+    ]
+
+    # order by mean_trait_rarity
+    toucan_rarity = toucan_rarity.sort_values(by=["mean_trait_rarity"], ascending=False)
+
+    return html.Div(
+        [
+            html.H1("Toucan Rarity"),
+            html.Hr(),
+            # use dbc to make a table of sprints
+            dbc.Table.from_dataframe(
+                toucan_rarity,
+                striped=True,
+                bordered=True,
+                hover=True,
+                responsive=True,
+            ),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(port=8888)
